676ac634c68791eb94fd0c8d3ff39b993f5f52a1
Kotaro0722 <kotaro.tanaka@4nonome.com>
2022-12-29 15:24:38 +0900

    NewsAndEventsを作成中 ファイルの作成

---
+++ src/stories/templates-newsandevents.stories.js
@@ -0,0 +1,6 @@
+import Item_NewsAndEvents from "../components/templates/news-and-events/newsAndEvents.html";
+import "../components/templates/news-and-events/newsAndEvents.scss";
+
+export default { title: "Templates/NewsAndEvents" };
+
+export const NewsAndEvents = () => Item_NewsAndEvents;
